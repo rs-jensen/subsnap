1f67dc83bac7f477f0342a20e75335f3b1c7b248
Rasmus-Stisen <rasmusstisen@pm.me>
2026-07-10 12:17:07 +0000

    Add tuple return type to cross_correlation function

--- correlate.h
+++ correlate.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <iostream>
 #include <limits>
+#include <tuple>
+#include <utility>
 
 std::pair<double, double> linear_regression(std::vector<double> x, std::vector<double> y, std::vector<double> w);
 std::tuple<double, double, float> cross_correlation(std::vector<int> activity_profile, std::vector<float> RMS);
