a02e32d34a069d3cbfddd9f5650357e392402776
Rasmus-Stisen <rasmusstisen@pm.me>
2026-07-13 20:36:37 +0000

    Update correlate.h

--- correlate.h
+++ correlate.h
@@ -4,6 +4,7 @@
 #include <limits>
 #include <tuple>
 #include <utility>
+#include <fftw3.h>
 
 std::pair<double, double> linear_regression(std::vector<double> x, std::vector<double> y, std::vector<double> w);
 std::tuple<double, double, float> cross_correlation(std::vector<int> activity_profile, std::vector<float> RMS);
