ded7e632ec278b6574bbd283a20a64589e9ab3e0
Rasmus-Stisen <rasmusstisen@pm.me>
2026-07-23 12:27:27 +0000

    Update correlate.h

--- engine/correlate.h
+++ engine/correlate.h
@@ -8,7 +8,3 @@
 
 std::pair<double, double> linear_regression(std::vector<double> x, std::vector<double> y, std::vector<double> w);
 std::pair<double, double> fft_crosscorrelate(std::vector<int> activity_profile, std::vector<int> srt_profile);
-<<<<<<< HEAD:correlate.h
-=======
-
->>>>>>> d55d3db (Move subtitle timestamp shifting from Python to C++, misc bug fixes):engine/correlate.h
